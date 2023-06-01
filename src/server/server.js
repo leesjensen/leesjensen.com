24ef80e7089a5418830db7e18c963957de95c860
Lee <lee.jensen@agilix.com>
2023-06-01 10:33:47 -0600

    moving to Vite

--- src/server/server.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const server = express();
-server.use(express.static('dist'));
-
-server.get('/', express.static('app', { index: "index.html"}));
-
-server.listen(4242, () => console.log('Server is running on http://localhost:4242 ...'));
